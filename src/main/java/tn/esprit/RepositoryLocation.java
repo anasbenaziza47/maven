7a2dc20e1220c45b89d7108f108d20ab60f91346
anasbenaziza47 <anas.benaziza@esprit.tn>
2021-04-07 20:03:22 +0200

    first commit

---
+++ src/main/java/tn/esprit/RepositoryLocation.java
@@ -0,0 +1,18 @@
+package tn.esprit;
+import java.io.File;
+
+public class RepositoryLocation {
+
+	public static void main(String[] args) {
+		File file = new
+				File("C:\\Users\\Anas PC\\.m2\\repository\\tn\\esprit\\tpMavenWithJarr\\1.0");
+				String absolutePath = file.getAbsolutePath();
+				String filePath = absolutePath.
+				substring(0,absolutePath.indexOf(File.separator, 30));
+				System.out.println("Le dossier contenant les dépendances "
+				+ "et les livrables des projets est : ");
+				System.out.println( filePath);
+
+	}
+
+}
